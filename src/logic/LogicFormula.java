cdab2ae6f5d3bbae73564af16150bce09ccbbdb3
tnguyen57 <44514418+tnguyen57@users.noreply.github.com>
2020-06-30 20:32:20 -0500

    Added placeholder for LogicFormula class.

---
+++ src/logic/LogicFormula.java
@@ -0,0 +1,19 @@
+package logic;
+
+/**
+ * Represents a logic formula in first order logic
+ * @author nguyet15
+ */
+public class LogicFormula {
+	boolean isPremise;
+	
+	/**
+	 * Contructor for the logic formula class. 
+	 * Takes in a string the represents a formula and process it.
+	 * @param formula: A string representing a formula in first order logic.
+	 */
+	LogicFormula(String formula){
+		;
+	}
+
+}
