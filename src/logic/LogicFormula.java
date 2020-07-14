c9b46200804a0c01c33687092bae687e7b0de34e
tnguyen57 <44514418+tnguyen57@users.noreply.github.com>
2020-07-14 11:47:50 -0500

    Added more skeleton code to Logic Formula.

--- src/logic/LogicFormula.java
+++ src/logic/LogicFormula.java
@@ -10,22 +10,39 @@ public class LogicFormula {
 	boolean isPremise= false;
 	TreeNode node = null;
 	
+	private int id = 0;
+	
 	/**
 	 * Constructor for a blank logic formula class.
+	 * @param id: The id of the LogicFormula.
 	 * @effect: Create a non-premise empty LogicFormula class 
 	 */
-	LogicFormula() {
+	public LogicFormula(int id) {
+		this.id = id;
 		//TO DO
+		;
+	}
+	
+	/**
+	 * Constructor for a blank logic formula class. Can specify whether premise or not.
+	 * @param id: The id of the LogicFormula.
+	 * @effect: Create a premise or non-premise empty LogicFormula class
+	 */
+	public LogicFormula(int id, boolean isPremise){
+		//TO DO
+		this.id = id;
+		this.isPremise = isPremise;
 		;
 	}
 	
 	/**
 	 * Constructor for the logic formula class. 
 	 * Takes in a string the represents a formula and process it.
+	 * @param id: The id of the LogicFormula.
 	 * @param formula: A string representing a formula in first order logic.
-	 * 
 	 */
-	LogicFormula(String formula){
+	public LogicFormula(int id, String formula){
+		this.id = id;
 		// TO DO
 		;
 	}
@@ -37,6 +54,24 @@ public class LogicFormula {
 	 */
 	public void addNode(TreeNode node) {
 		this.node = node;
+	}
+	
+	/**
+	 * Edit the formula with the new string
+	 * @param formula: The new formula to be created
+	 * @effect: Change the formula to the new formula provided
+	 */
+	public void edit(String formula) {
+		//TO DO
+		;
+	}
+	
+	/**
+	 * Getter for the id of the LogicFormula
+	 * @returns: Returns the id of the LogicFormula.
+	 */
+	public int getID() {
+		return this.id;
 	}
 
 }
